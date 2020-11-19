1d981381937337b79f2dd40f2a37a967cc154742
andrewmiddleton1 <66552488+andrewmiddleton1@users.noreply.github.com>
2020-11-19 20:47:20 +1100

    2nd version of DB schema

--- schema.sql
+++ schema.sql
@@ -8,6 +8,8 @@ CREATE TABLE app_user
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(50) NOT NULL,
+    email varchar (50) NOT NULL,
+    password varchar (50) NOT NULL,
     age INT,
     location varchar (50) NOT NULL,
     avatar varchar (200),
@@ -19,7 +21,6 @@ CREATE TABLE user_chat
 	id int NOT NULL AUTO_INCREMENT,
 	message varchar(1000),
     user_id INT,
-    PRIMARY KEY (id),
-    INDEX us_ind (user_id),
-    CONSTRAINT fk_app_user FOREIGN KEY (user_id) REFERENCES app_user(id)
+    PRIMARY KEY (id),    
+    FOREIGN KEY (user_id) REFERENCES app_user(id)
 );
