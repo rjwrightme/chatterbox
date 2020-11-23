290b38c25f2c733c50b60f273f384c1b45e8bba4
Annisa Purbandari <a.purbandari@gmail.com>
2020-11-23 21:07:02 +1100

    user login authentication

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,6 +6,10 @@
 // =============================================================
 var path = require("path");
 
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+
 // Routes
 // =============================================================
 module.exports = function (app) {
@@ -14,8 +18,23 @@ module.exports = function (app) {
 
   // index route loads view.html
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/chat.html"));
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
+
+  app.get("/login", function (req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
 
   // cms route loads cms.html
   app.get("/cms", function (req, res) {
--- server.js
+++ server.js
@@ -7,6 +7,11 @@ var port = process.env.PORT || 3600;
 var server = http.createServer(app);
 var io = require("socket.io")(server);
 
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static("public"));
 
 
@@ -22,6 +27,11 @@ app.set("view engine", "handlebars");
 
 require("./controllers/chat_controller.js")(app);
 
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/chat-api-routes.js")(app);
+require("./routes/login-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
 
 
 io.on("connection", (socket) => {
@@ -34,4 +44,11 @@ io.on("connection", (socket) => {
 
 server.listen(port, function () {
   console.log("listen to" + port);
+});
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in our browser.", PORT, PORT);
+  });
 });
