d3f910f7c6eebc1c639e5683da3a40a52347a9c5
RJ Wright <rj@rjwright.me>
2020-11-24 20:41:08 +1100

    Quick server fix from broken merge conflict

--- utils/users.js
+++ utils/users.js
@@ -2,35 +2,35 @@ const users = [];
 
 // Join user to chat
 function userJoin(id, username, room) {
-    const user = { id, username, room };
+  const user = { id, username, room };
 
-    users.push(user);
+  users.push(user);
 
-    return user;
+  return user;
 }
 
 // Get current user
 function getCurrentUser(id) {
-    return users.find(user => user.id === id);
+  return users.find(user => user.id === id);
 }
 
 // User leaves chat
 function userLeave(id) {
-    const index = users.findIndex(user => user.id === id);
+  const index = users.findIndex(user => user.id === id);
 
-    if (index !== -1) {
-        return users.splice(index, 1)[0];
-    }
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
 }
 
 // Get room users
 function getRoomUsers(room) {
-    return users.filter(user => user.room === room);
+  return users.filter(user => user.room === room);
 }
 
 module.exports = {
-    userJoin,
-    getCurrentUser,
-    userLeave,
-    getRoomUsers
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
 };
