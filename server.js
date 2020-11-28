86f9904ad7364c50e34e88f3cbaaa499812ef27b
Annisa Purbandari <a.purbandari@gmail.com>
2020-11-28 16:00:00 +1100

    fixed double chat

--- server.js
+++ server.js
@@ -97,13 +97,6 @@ io.on("connection", (socket) => {
     io.to(user.room).emit("message", formatMessage(user.username, msg));
   });
 
-  // Listen for chatMessage
-  socket.on("chatMessage", (msg) => {
-    const user = getCurrentUser(socket.id);
-
-    io.to(user.room).emit("message", formatMessage(user.username, msg));
-  });
-
   // Runs when client disconnects
   socket.on("disconnect", () => {
     const user = userLeave(socket.id);
